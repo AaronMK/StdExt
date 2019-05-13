a6e22ac44f58ec7cc99faad6a6cdd42c6a81fa74
Aaron Karp <aaron.m.karp@gmail.com>
2019-05-13 10:52:24 -0400

    Add can_assign_from and additional properties for Traits<T> and Type.

--- include/StdExt/Type.h
+++ include/StdExt/Type.h
@@ -11,6 +11,47 @@
 
 namespace StdExt
 {
+	template<typename T = void>
+	struct assign
+	{
+		constexpr void operator()(T left, T right)
+		{
+			left = right;
+		}
+	};
+
+	template<>
+	struct assign<void>
+	{
+		template<typename left_t, typename right_t>
+		constexpr void operator()(left_t left, const right_t right)
+		{
+			left = right;
+		}
+	};
+
+#pragma region can_assign_from
+	template<typename target_t, typename... _Types>
+	struct can_assign_from;
+
+	template<typename target_t, typename value_t>
+	struct can_assign_from<target_t, value_t>
+	{
+		static constexpr bool value = std::is_convertible_v<value_t, target_t>;
+	};
+
+	template<typename target_t, typename a_t, typename b_t, typename... rest_t>
+	struct can_assign_from<target_t, a_t, b_t, rest_t...>
+	{
+		static constexpr bool value =
+			can_assign_from<target_t, a_t>::value &&
+			can_assign_from<target_t, b_t, rest_t...>::value;
+	};
+
+	template<typename target_t, typename a_t, typename... rest_t>
+	constexpr bool can_assign_from_v = can_assign_from<target_t, a_t, rest_t...>::value;
+#pragma endregion
+
 	template<typename T>
 	struct Traits
 	{
@@ -27,11 +68,16 @@ namespace StdExt
 		static constexpr bool copy_assignable = std::is_copy_assignable_v<T>;
 		static constexpr bool move_assignable = std::is_move_assignable_v<T>;
 
+		static constexpr bool is_void = std::is_void_v<T>;
 		static constexpr bool is_arithmetic = std::is_arithmetic_v<T>;
 		static constexpr bool is_signed = std::is_signed_v<T>;
 		static constexpr bool is_unsigned = std::is_unsigned_v<T>;
 		static constexpr bool is_pointer = std::is_pointer_v<T>;
+		static constexpr bool is_const = std::is_const_v<T>;
+		static constexpr bool is_reference = std::is_lvalue_reference_v<T>;
+		static constexpr bool is_move_reference = std::is_rvalue_reference_v<T>;
 
+		static constexpr bool is_class = std::is_class_v<T>;
 
 		static T default_value()
 		{
@@ -68,6 +114,14 @@ namespace StdExt
 			virtual bool canCopyAssign() const = 0;
 			virtual bool canMoveAssign() const = 0;
 
+			virtual bool isVoid() const = 0;
+			virtual bool isConstant() const = 0;
+			virtual bool isPointer() const = 0;
+			virtual bool isReference() const = 0;
+			virtual bool isMove() const = 0;
+
+			virtual bool isClass() const = 0;
+
 			virtual String name() const = 0;
 
 			virtual const std::type_info& stdTypeInfo() const = 0;
@@ -79,28 +133,60 @@ namespace StdExt
 		{
 			virtual bool canDefaultConstruct() const override
 			{
-				return std::is_default_constructible_v<T>;
+				return Traits<T>::default_constructable;
 			}
 
 			virtual bool canCopy() const override
 			{
-				return std::is_copy_constructible_v<T>;
+				return Traits<T>::copy_constructable;
 			}
 
 			virtual bool canMove() const override
 			{
-				return std::is_move_constructible_v<T>;
+				return Traits<T>::move_constructable;
 			}
 
 			virtual bool canCopyAssign() const override
 			{
-				return std::is_copy_assignable_v<T>;
+				return Traits<T>::copy_assignable;
 			}
 
 			virtual bool canMoveAssign() const override
 			{
-				return std::is_move_assignable_v<T>;
+				return Traits<T>::move_assignable;
 			}
+
+			virtual bool isVoid() const  override
+			{
+				return Traits<T>::is_void;
+			}
+
+			virtual bool isConstant() const  override
+			{
+				return Traits<T>::is_const;
+			}
+
+			virtual bool isPointer() const  override
+			{
+				return Traits<T>::is_pointer;
+			}
+
+			virtual bool isReference() const  override
+			{
+				return Traits<T>::is_reference;
+			}
+
+			virtual bool isMove() const  override
+			{
+				return Traits<T>::is_move_reference;
+			}
+
+
+			virtual bool isClass() const  override
+			{
+				return Traits<T>::is_class;
+			}
+
 
 			virtual String name() const override
 			{
@@ -163,6 +249,36 @@ namespace StdExt
 		bool canMoveAssign() const
 		{
 			return mTypeImp->canMoveAssign();
+		}
+
+		bool isVoid() const
+		{
+			return mTypeImp->isVoid();
+		}
+
+		bool isConstant() const
+		{
+			return mTypeImp->isConstant();
+		}
+
+		bool isPointer() const
+		{
+			return mTypeImp->isPointer();
+		}
+
+		bool isReference() const
+		{
+			return mTypeImp->isReference();
+		}
+
+		bool isMove() const
+		{
+			return mTypeImp->isMove();
+		}
+
+		bool isClass() const
+		{
+			return mTypeImp->isClass();
 		}
 
 		String name() const
