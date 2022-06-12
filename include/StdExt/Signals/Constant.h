bcc02ba2173ab12976d15517565401e70e957716
Aaron Karp <aaron.m.karp@gmail.com>
2022-06-12 19:46:44 -0400

    Update ConstWatchable.

--- include/StdExt/Signals/Constant.h
+++ include/StdExt/Signals/Constant.h
@@ -11,34 +11,16 @@ namespace StdExt::Signals
 		using base_t = Watchable<T>;
 
 	public:
-		ConstWatchable(const T& value);
-		ConstWatchable(T&& value);
+		ConstWatchable(const T& value)
+		{
+			base_t::updateValue(value);
+		}
 
-	protected:
-		virtual T calcValue() const override;
+		ConstWatchable(T&& value)
+		{
+			base_t::updateValue(std::move(value));
+		}
 	};
-
-	////////////////////////////////////
-
-	template<typename T>
-	ConstWatchable<T>::ConstWatchable(const T& value)
-	{
-		base_t::setQuickValue(true);
-		base_t::notify(value);
-	}
-
-	template<typename T>
-	ConstWatchable<T>::ConstWatchable(T&& value)
-	{
-		base_t::setQuickValue(true);
-		base_t::notify(value);
-	}
-
-	template<typename T>
-	T ConstWatchable<T>::calcValue() const
-	{
-		return base_t::lastSent();
-	}
 }
 
 #endif // !_STD_EXT_SIGNALS_CONSTANT_H_
