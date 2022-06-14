cb2c4649b9062de7fcb4e20a136749ed7f608fce
Aaron Karp <aaron.m.karp@gmail.com>
2022-06-14 18:59:07 -0400

    Add function pointer convenience defitions.

--- include/StdExt/FunctionPtr.h
+++ include/StdExt/FunctionPtr.h
@@ -5,11 +5,60 @@
 #include <algorithm>
 
 #include "Exceptions.h"
+#include "Concepts.h"
 #include "Memory.h"
 #include "Utility.h"
 
 namespace StdExt
 {
+	/**
+	 * @brief
+	 *  Convenience definition of a member function pointer to avoid syntax that can
+	 *  be messy and hard to remember.
+	 * 
+	 * @tparam class_t
+	 *  The class of the member function.
+	 * 
+	 * @tparam return_t
+	 *  return_t The return type of the function.
+	 * 
+	 * @tparam args_t
+	 *  The types of the arguments.
+	 */
+	template<Class class_t, typename return_t, typename ...args_t>
+	using MemberFunctionPtr = return_t (class_t::*)(args_t...);
+
+	/**
+	 * @brief
+	 *  Convenience definition of a const member function pointer to avoid syntax
+	 *  that can be messy and hard to remember.
+	 *
+	 * @tparam class_t
+	 *  The class of the member function.
+	 *
+	 * @tparam return_t
+	 *  return_t The return type of the function.
+	 *
+	 * @tparam args_t
+	 *  The types of the arguments.
+	 */
+	template<Class class_t, typename return_t, typename ...args_t>
+	using ConstMemberFunctionPtr = return_t (class_t::*)(args_t...) const;
+
+	/**
+	 * @brief
+	 *  Convenience definition of a static function pointer to avoid syntax
+	 *  that can be messy and hard to remember.
+	 *
+	 * @tparam return_t
+	 *  return_t The return type of the function.
+	 *
+	 * @tparam args_t
+	 *  The types of the arguments.
+	 */
+	template<typename return_t, typename ...args_t>
+	using StaticFunctionPtr = return_t (*)(args_t...);
+
 	/**
 	 * @brief
 	 *  This class stores a function pointer of any type and provides
