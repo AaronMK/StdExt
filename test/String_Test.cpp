113e1b72c4e796870854fa0c9539fa2ec1af8b15
Aaron Karp <aaron.m.karp@gmail.com>
2022-08-23 18:26:51 -0400

    Formatting

--- test/String_Test.cpp
+++ test/String_Test.cpp
@@ -28,8 +28,8 @@ static auto testConversion(const StringBase<in_t>& input)
 		msg.str(), cvt_in,
 		convertString<from_t>(
 			convertString<to_t>(cvt_in)
-			)
-		);
+		)
+	);
 }
 
 void testString()
@@ -85,90 +85,90 @@ void testString()
 	Test::testForResult<bool>(
 		"strLongString initially null-terminated.",
 		true, strLongString.isNullTerminated()
-		);
+	);
 
 	Test::testForResult<bool>(
 		"strSmallString initially null-terminated.",
 		true, strSmallString.isNullTerminated()
-		);
+	);
 
 	Test::testForResult<bool>(
 		"strLongString is not local",
 		false, strLongString.isLocal()
-		);
+	);
 
 	Test::testForResult<bool>(
 		"strLongString is stored on the heap.",
 		true, strLongString.isOnHeap()
-		);
+	);
 
 	Test::testForResult<bool>(
 		"strSmallString is local",
 		true, strSmallString.isLocal()
-		);
+	);
 
 	Test::testForResult<bool>(
 		"strSmallString is not stored on the heap.",
 		false, strSmallString.isOnHeap()
-		);
+	);
 
 	Test::testForResult<bool>(
 		"litLongString is not stored on the heap.",
 		false, litLongString.isOnHeap()
-		);
+	);
 
 	Test::testForResult<bool>(
 		"litSmallString is not stored on the heap.",
 		false, litSmallString.isOnHeap()
-		);
+	);
 
 	Test::testForResult<bool>(
 		"litLongString is not local.",
 		false, litLongString.isLocal()
-		);
+	);
 
 	Test::testForResult<bool>(
 		"litSmallString is not local.",
 		false, litSmallString.isLocal()
-		);
+	);
 
 	U8String subStr = strLongString.substr(3, 3);
 
 	Test::testForResult<int>(
 		"subStr() returns correct result with valid parameters.",
 		0, compare(subStr, u8"DEF")
-		);
+	);
 
 	Test::testForResult<bool>(
 		"subStr() returns a string stored internally since the length is small.",
 		true, subStr.isLocal()
-		);
+	);
 
 	subStr = strLongString.substr(3, 18);
 
 	Test::testForResult<int>(
 		"subStr() returns correct result with valid parameters.",
 		0, compare(subStr, u8"DEFGHIJKLMNOPQRSTU")
-		);
+	);
 
 	Test::testForResult<bool>(
 		"subStr references same memory as original for long string.",
 		true, memory_overlaps(
 			subStr.data(), subStr.size(),
 			strLongString.data(), strLongString.size())
-		);
+	);
 
 	Test::testForResult<bool>(
 		"subStr is not null terminated since it references part of another string.",
 		false, subStr.isNullTerminated()
-		);
+	);
 
 	U8String nullTerminated = subStr.getNullTerminated();
 
 	Test::testForResult<bool>(
 		"getNullTerminated() returns a null terminated string.",
 		true, nullTerminated.isNullTerminated()
-		);
+	);
 
 	Test::testForResult<bool>(
 		"nullTerminated references different memory as original "
@@ -176,14 +176,14 @@ void testString()
 		false, memory_overlaps(
 			nullTerminated.data(), nullTerminated.size(),
 			strLongString.data(), strLongString.size())
-		);
+	);
 
 	subStr = strLongString.substr(3, 23);
 
 	Test::testForResult<bool>(
 		"subStr null terminated since it goes to the end of the original string.",
 		true, subStr.isNullTerminated()
-		);
+	);
 
 	nullTerminated = subStr.getNullTerminated();
 
@@ -191,5 +191,5 @@ void testString()
 		"nullTerminated references same memory as original "
 		"since original was also null-terminated.",
 		true, nullTerminated.data() == subStr.data()
-		);
+	);
 }
