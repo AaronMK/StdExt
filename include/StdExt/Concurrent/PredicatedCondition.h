effa5faa2630ca21433c6c3c61ea9ae08b96ed76
Aaron Karp <aaron.m.karp@gmail.com>
2022-10-16 18:19:41 -0400

    Adding documentation.

--- include/StdExt/Concurrent/PredicatedCondition.h
+++ include/StdExt/Concurrent/PredicatedCondition.h
@@ -20,6 +20,25 @@
 
 namespace StdExt::Concurrent
 {
+	/**
+	 * @brief
+	 *  A manual reset condition that faclitates thread-safe interaction with protected data,
+	 *  and allows specification of conditional wakeup parameters.
+	 * 
+	 * @brief
+	 *  This class has the following distinctions from std::condition_variable:
+	 * 
+	 *   - It is aware of and works with task parallel and thread pool frameworks of
+	 *     the target platform.
+	 * 
+	 *   - Manual reset functionality.
+	 * 
+	 *   - Isolated invokation of predicates and actions when threads are woken up are handled
+	 *     by the wait calls, making an externally managed mutex for call isolation unnecessary.
+	 * 
+	 *   - A destroy() function is provided allowing client code to block further wait calls,
+	 *     and know that any active wait calls will have completed before destroy returns.
+	 */
 	class PredicatedCondition
 	{
 	private:
@@ -161,18 +180,36 @@ namespace StdExt::Concurrent
 
 		/**
 		 * @brief
-		 *  Wakes one waiting cleint whose predicate is satisfied.
+		 *  Wakes one waiting client whose predicate is satisfied.  The triggered
+		 *  state will be set to true to wake a waiting thread, when back to
+		 *  its original state once that has been completed.
+		 * 
+		 * @param action
+		 *  An action taken in the calling thread and done atomically with
+		 *  repsect to other actions and predicates passed to wait calls.
 		 * 
 		 * @returns
-		 *  True if a client passes its predicate and is woken up.
-		 *  False otherwise.
+		 *  True if a client passes its predicate and is woken up,
+		 *  false otherwise.
 		 */
 		template<CallableWith<void> action_t>
 		bool triggerOne(const action_t& action)
 		{
 			lock_t lock(mMutex);
-			
+
+			if ( mDestroyed )
+				throw object_destroyed("triggerOne() called on destroyed PredicatedCondition.");
+
+			bool old_active = mActive;
 			mActive = true;
+
+			auto reset_active = finalBlock(
+				[old_active]()
+				{
+					mActive = old_active;
+				}
+			);
+
 			action();
 
 			for (size_t i = 0; i < mWaitQueue.size(); ++i)
@@ -187,10 +224,26 @@ namespace StdExt::Concurrent
 			return false;
 		}
 		
+		/**
+		 * @brief
+		 *  Sets the triggered state to true (if not already) after calling action, and 
+		 *  wakes all waiting threads whose predictes are satisfied.
+		 * 
+		 * @param action
+		 *  An action taken in the calling thread and done atomically with
+		 *  repsect to other actions and predicates passed to wait calls.
+		 * 
+		 * @returns
+		 *  The number of waiting threads that were awoken.  Note: this number will not
+		 *  reflect counts of awakenings as a result recursive trigger calls.
+		 */
 		template<CallableWith<void> action_t>
 		size_t triggerAll(const action_t& action)
 		{
 			lock_t lock(mMutex);
+
+			if ( mDestroyed )
+				throw object_destroyed("triggerAll() called on destroyed PredicatedCondition.");
 			
 			mActive = true;
 			action();
@@ -209,16 +262,67 @@ namespace StdExt::Concurrent
 			return clients_triggered;
 		}
 
+		/**
+		 * @brief
+		 *  Wakes one waiting client whose predicate is satisfied.  The triggered
+		 *  state will be set to true to wake a waiting thread, when back to
+		 *  its original state once that has been completed.
+		 * 
+		 * @returns
+		 *  True if a client passes its predicate and is woken up,
+		 *  false otherwise.
+		 */
 		bool triggerOne()
 		{
 			return triggerOne( []() {} );
 		}
 
+		/**
+		 * @brief
+		 *  Sets the triggered state to true, waking all waiting threads whose
+		 *  predictes are satisfied.
+		 * 
+		 * @returns
+		 *  The number of waiting threads that were awoken.  Note: this number will not
+		 *  reflect counts of awakenings as a result recursive trigger calls.
+		 */
 		size_t triggerAll()
 		{
 			return triggerAll( []() {} );
 		}
+
+		/**
+		 * @brief
+		 *  Resets the triggered state of the condition, preventing waiting threads from being
+		 *  awoken until a trigger function is called or the condition is destroyed.
+		 */
+		void reset()
+		{
+			lock_t lock(mMutex);
+
+			mActive = false;
+		}
 		
+		/**
+		 * @brief
+		 *  Waits for this condition to be triggered and for a predicate to be satisfied
+		 *  before taking an action in the calling thread.
+		 * 
+		 * @param timeout
+		 *  The amount of time to wait.
+		 * 
+		 * @param predicate
+		 *  Used to test for a precondition for taking an action.  This can be done in the calling
+		 *  thread, or in the context of trigger calls to determine which threads to awaken.
+		 * 
+		 * @param action
+		 *  An action taken in within the calling thread if the condition is triggered and
+		 *  the predicate is met within the timeout.
+		 * 
+		 * @throws
+		 *  A time_out exception on timeouts, or an object_destroyed exception if the condition
+		 *  is destroyed.
+		 */
 		template<CallableWith<bool> predicate_t, CallableWith<void> action_t>
 		void wait(timeout_t timeout, predicate_t&& predicate, const action_t& action)
 		{
@@ -252,6 +356,14 @@ namespace StdExt::Concurrent
 			action();
 		}
 		
+		/**
+		 * @brief
+		 *  Waits for the condition to be in a triggered state.
+		 * 
+		 * @throws
+		 *  An object_destroyed exception if the condition is destroyed before
+		 *  being triggered.
+		 */
 		bool wait()
 		{
 			wait(
@@ -260,7 +372,19 @@ namespace StdExt::Concurrent
 				[]() {}
 			);
 		}
-
+		
+		/**
+		 * @brief
+		 *  Waits for, at most, a specifed amount of time for the condition to be
+		 *  in a triggered state.
+		 * 
+		 * @param timeout
+		 *  The amount of time to wait.
+		 * 
+		 * @throws
+		 *  A time_out exception on timeouts, or an object_destroyed exception if the condition
+		 *  is destroyed.
+		 */
 		bool wait(timeout_t timout)
 		{
 			wait(
@@ -270,6 +394,21 @@ namespace StdExt::Concurrent
 			);
 		}
 		
+		/**
+		 * @brief
+		 *  Waits for this condition to be triggered and for a predicate to be satisfied.
+		 * 
+		 * @param timeout
+		 *  The amount of time to wait.
+		 * 
+		 * @param predicate
+		 *  Used to test for a precondition for taking an action.  This can be done in the calling
+		 *  thread, or in the context of trigger calls to determine which threads to awaken.
+		 * 
+		 * @throws
+		 *  A time_out exception on timeouts, or an object_destroyed exception if the condition
+		 *  is destroyed.
+		 */
 		template<CallableWith<bool> predicate_t>
 		void wait(timeout_t timeout, predicate_t&& predicate)
 		{
@@ -279,7 +418,24 @@ namespace StdExt::Concurrent
 				[]() {}
 			);
 		}
-
+		
+		
+		/**
+		 * @brief
+		 *  Waits for this condition to be triggered before taking an action in the
+		 *  calling thread.
+		 * 
+		 * @param timeout
+		 *  The amount of time to wait.
+		 * 
+		 * @param action
+		 *  An action taken in within the calling thread if the condition is triggered 
+		 *  within the timeout.
+		 * 
+		 * @throws
+		 *  A time_out exception on timeouts, or an object_destroyed exception if the condition
+		 *  is destroyed.
+		 */
 		template<CallableWith<void> action_t>
 		void wait(timeout_t timeout, const action_t& action)
 		{
@@ -290,6 +446,15 @@ namespace StdExt::Concurrent
 			);
 		}
 		
+		/**
+		 * @brief
+		 *  Waits for this condition to be triggered and for a predicate to be satisfied
+		 *  before returning to the calling thread.
+		 * 
+		 * @param predicate
+		 *  Used to test for a precondition for taking an action.  This can be done in the calling
+		 *  thread, or in the context of trigger calls to determine which threads to awaken.
+		 */
 		template<CallableWith<bool> predicate_t>
 		void wait(predicate_t&& predicate)
 		{
@@ -300,6 +465,17 @@ namespace StdExt::Concurrent
 			);
 		}
 		
+		/**
+		 * @brief
+		 *  Waits for this condition to be triggered before taking an action in the calling thread.
+		 * 
+		 * @param action
+		 *  An action taken in within the calling thread if the condition is triggered and
+		 *  the predicate is met within the timeout.
+		 * 
+		 * @throws
+		 *  An object_destroyed exception if the condition is destroyed.
+		 */
 		template<CallableWith<void> action_t>
 		void wait(const action_t& action)
 		{
@@ -309,7 +485,23 @@ namespace StdExt::Concurrent
 				action
 			);
 		}
-
+		
+		/**
+		 * @brief
+		 *  Waits for this condition to be triggered and for a predicate to be satisfied
+		 *  before taking an action in the calling thread.
+		 * 
+		 * @param predicate
+		 *  Used to test for a precondition for taking an action.  This can be done in the calling
+		 *  thread, or in the context of trigger calls to determine which threads to awaken.
+		 * 
+		 * @param action
+		 *  An action taken in within the calling thread if the condition is triggered and
+		 *  the predicate is met.
+		 * 
+		 * @throws
+		 *  An object_destroyed exception if the condition is destroyed.
+		 */
 		template<CallableWith<bool> predicate_t, CallableWith<void> action_t>
 		void wait(predicate_t&& predicate, const action_t& action)
 		{
