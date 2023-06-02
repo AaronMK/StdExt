a80628afb9bafcde6a810caf40718846c4747eda
Aaron Karp <aaron.m.karp@gmail.com>
2023-06-02 00:08:31 -0400

    Minor code update.

--- include/StdExt/Concurrent/Condition.h
+++ include/StdExt/Concurrent/Condition.h
@@ -109,6 +109,9 @@ namespace StdExt::Concurrent
 	 * @param timeout
 	 *  The amount of time to wait.
 	 * 
+	 * @param test_func
+	 *  Function used to test whether some criteria has been met.
+	 * 
 	 * @return
 	 *  True if the test function evaluates to true before the timeout, false otherwise.
 	 */
