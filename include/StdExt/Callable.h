688075ce07811ea1fd148208df660b1ab300c636
Aaron Karp <aaron.m.karp@gmail.com>
2023-10-24 20:24:36 -0400

    Callable is working without a helper "make" function.

--- include/StdExt/Callable.h
+++ include/StdExt/Callable.h
@@ -3,8 +3,13 @@
 
 #include "CallableTraits.h"
 
+#include "Concepts.h"
+
 namespace StdExt
 {
+	template<typename ret_t, typename... args_t>
+	class Callable;
+
 	template<typename ret_t, typename... args_t>
 	class Callable
 	{
@@ -20,47 +25,59 @@ namespace StdExt
 				return run(std::forward<args_t>(args)...);
 		}
 
-		template<typename callable_t>
-		class Implementation : public Callable<ret_t, args_t...>
-		{
-		private:
-			callable_t mCallable;
-		public:
-			Implementation(const callable_t& callable)
-				requires CopyConstructable<callable_t>
-				: mCallable(callable)
-			{
-			}
-
-			Implementation(callable_t&& callable)
-				requires MoveConstructable<callable_t>
-				: mCallable( std::move(callable) )
-			{
-			}
-
-		protected:
-			ret_t run(args_t... args) const override
-			{
-				
-			if constexpr ( std::is_void_v<ret_t> )
-				mCallable(std::forward<args_t>(args)...);
-			else
-				return mCallable(std::forward<args_t>(args)...);
-			}
-		};
-
 	protected:
 		virtual ret_t run(args_t... args) const = 0;
 	};
 
-	template<typename callable_t>
-	auto makeCallable(callable_t&& func)
+	template<typename callable_t, typename ret_t, typename... args_t>
+		requires ( CallableWith<callable_t, ret_t, args_t...> )
+	class Callable<callable_t, ret_t, args_t...> : public Callable<ret_t, args_t...>
 	{
-		using call_t = CallableTraits<callable_t>::template forward<Callable>;
-		using imp_t  = call_t::template Implementation<callable_t>;
+	public:
+		Callable(const callable_t& callable)
+			requires CopyConstructable<callable_t>
+			: mCallable(callable)
+		{
+		}
 
-		return imp_t(std::forward<callable_t>(func));
-	}
+		Callable(callable_t&& callable)
+			requires MoveConstructable<callable_t>
+			: mCallable( std::move(callable) )
+		{
+		}
+
+	protected:
+		ret_t run(args_t... args) const override
+		{
+			if constexpr ( std::is_void_v<ret_t> )
+				mCallable(std::forward<args_t>(args)...);
+			else
+				return mCallable(std::forward<args_t>(args)...);
+		}
+
+	private:
+		mutable callable_t mCallable;
+	};
+
+	template<typename callable_t>
+	class Callable<callable_t> : public CallableTraits<callable_t>::template forward<Callable, callable_t>
+	{
+	public:
+		using base_t = typename CallableTraits<callable_t>::template forward<Callable, callable_t>;
+
+		Callable(const callable_t& func)
+			: base_t(func)
+		{
+		}
+
+		Callable(callable_t&& func)
+			: base_t(std::move(func))
+		{
+		}
+	};
+
+	template<typename callable_t>
+	Callable(callable_t func) -> Callable<callable_t>;
 }
 
 #endif // !_STD_EXT_CALLABLE_H_
