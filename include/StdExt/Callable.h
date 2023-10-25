4831b31d5335baade1abf4af47076125cf17949e
Aaron Karp <aaron.m.karp@gmail.com>
2023-10-25 08:17:56 -0400

    CallableRef throws nullptr exception when applicable.

--- include/StdExt/Callable.h
+++ include/StdExt/Callable.h
@@ -4,10 +4,116 @@
 #include "CallableTraits.h"
 
 #include "Concepts.h"
+#include "Exceptions.h"
 #include "Utility.h"
+
 
 namespace StdExt
 {
+	/**
+	 * @brief
+	 *  Stores a pointer to a callable type and the code to forward invokations of its
+	 *  own call operator to the callable object to which it currently points.
+	 *
+	 * @details
+	 *  Some use cases:
+	 *
+	 *  A parameter type for functions that can take a pointer to a callable without needing
+	 *  to template the function, or rely on std::function as a bridge and the potential
+	 *  memory allocation:
+	 * 
+	 * @code
+	 *	int foo(StdExt::CallableRef<int, int> func)
+	 *	{
+	 *		return func(1);
+	 *	}
+	 * 
+	 *	int tmp = foo(
+	 *		[](int i)
+	 *		{
+	 *			return i + 1;
+	 *		}
+	 *	);
+	 * @endcode
+	 * 
+	 *  A function pointer type that be dynamically set to point to direrent function types
+	 *  as on as they have the same signature:
+	 * 
+	 * @code
+	 *	auto lambda_plus_one = [](int i)
+	 *		{
+	 *			return i + 1;
+	 *		};
+	 *	
+	 *	auto lambda_plus_two = [](int i)
+	 *		{
+	 *			return i + 2;
+	 *		};
+	 *	
+	 *	StdExt::CallableRef<int, int> call_ref;
+	 *	
+	 *	call_ref = lambda_plus_one;
+	 *	int result = call_ref(1);
+	 *	
+	 *	call_ref = lambda_plus_two;
+	 *	result = call_ref(1);
+	 * @endcode
+	 * 
+	 * Some notes:
+	 *  - Encapsuting pointers to non-static member functions are not supported.  The preferred method is to
+	 *    wrap the call into a lambda with the target object captured.
+	 * 
+	 *  - When using as a replacement for a templated parameter type, there is more overhead in making a
+	 *    function call.  It has to dereference both a pointer to the callable object and a pointer to
+	 *    a function that will implement the actual call, and fewer optimization oportunities.  However,
+	 *    this will allow for implmenting the calling code outside of a template definition.
+	 */
+	template<typename ret_t, typename... args_t>
+	class CallableRef
+	{
+	private:
+		using call_ptr_t = ret_t(*)(void*, args_t...);
+
+		void* mCallable;
+		call_ptr_t mCaller;
+		
+		template<typename callable_t>
+		static ret_t caller(void* func, args_t... args)
+		{
+			callable_t& func_ref = access_as<callable_t&>(func);
+
+			if constexpr ( std::is_void_v<ret_t> )
+				func_ref(std::forward<args_t>(args)...);
+			else
+				return func_ref(std::forward<args_t>(args)...);
+		}
+
+	public:
+		CallableRef()
+		{
+			mCaller   = nullptr;
+			mCallable = nullptr;
+		}
+
+		template<CallableWith<ret_t, args_t...> func_t>
+		CallableRef(const func_t& func)
+		{
+			mCaller = &caller<func_t>;
+			mCallable = access_as<void*>(&func);
+		}
+
+		ret_t operator()(args_t... args) const
+		{
+			if ( nullptr == mCallable )
+				throw null_pointer("Attempting to call null CallableRef");
+
+			if constexpr ( std::is_void_v<ret_t> )
+				mCaller(mCallable, std::forward<args_t>(args)...);
+			else
+				return mCaller(mCallable, std::forward<args_t>(args)...);
+		}
+	};
+
 	/**
 	 * @rbief
 	 *  Encapsulates a callable object or a function call and its
@@ -117,107 +223,6 @@ namespace StdExt
 
 	template<typename callable_t>
 	Callable(callable_t func) -> Callable<callable_t>;
-
-	/**
-	 * @brief
-	 *  Stores a pointer to a callable type and the code to forward invokations of its
-	 *  own call operator to the callable object to which it currently points.
-	 *
-	 * @details
-	 *  Some use cases:
-	 *
-	 *  A parameter type for functions that can take a pointer to a callable without needing
-	 *  to template the function, or rely on std::function as a bridge and the potential
-	 *  memory allocation:
-	 * 
-	 * @code
-	 *	int foo(StdExt::CallableRef<int, int> func)
-	 *	{
-	 *		return func(1);
-	 *	}
-	 * 
-	 *	int tmp = foo(
-	 *		[](int i)
-	 *		{
-	 *			return i + 1;
-	 *		}
-	 *	);
-	 * @endcode
-	 * 
-	 *  A function pointer type that be dynamically set to point to direrent function types
-	 *  as on as they have the same signature:
-	 * 
-	 * @code
-	 *	auto lambda_plus_one = [](int i)
-	 *		{
-	 *			return i + 1;
-	 *		};
-	 *	
-	 *	auto lambda_plus_two = [](int i)
-	 *		{
-	 *			return i + 2;
-	 *		};
-	 *	
-	 *	StdExt::CallableRef<int, int> call_ref;
-	 *	
-	 *	call_ref = lambda_plus_one;
-	 *	int result = call_ref(1);
-	 *	
-	 *	call_ref = lambda_plus_two;
-	 *	result = call_ref(1);
-	 * @endcode
-	 * 
-	 * Some notes:
-	 *  - Encapsuting pointers to non-static member functions are not supported.  The preferred method is to
-	 *    wrap the call into a lambda with the target object captured.
-	 * 
-	 *  - When using as a replacement for a templated parameter type, there is more overhead in making a
-	 *    function call.  It has to dereference both a pointer to the callable object and a pointer to
-	 *    a function that will implement the actual call, and fewer optimization oportunities.  However,
-	 *    this will allow for implmenting the calling code outside of a template definition.
-	 */
-	template<typename ret_t, typename... args_t>
-	class CallableRef
-	{
-	private:
-		using call_ptr_t = ret_t(*)(void*, args_t...);
-
-		void* mCallable;
-		call_ptr_t mCaller;
-		
-		template<typename callable_t>
-		static ret_t caller(void* func, args_t... args)
-		{
-			callable_t& func_ref = access_as<callable_t&>(func);
-
-			if constexpr ( std::is_void_v<ret_t> )
-				func_ref(std::forward<args_t>(args)...);
-			else
-				return func_ref(std::forward<args_t>(args)...);
-		}
-
-	public:
-		CallableRef()
-		{
-			mCaller   = nullptr;
-			mCallable = nullptr;
-		}
-
-		template<CallableWith<ret_t, args_t...> func_t>
-		CallableRef(const func_t& func)
-		{
-			mCaller = &caller<func_t>;
-			mCallable = access_as<void*>(&func);
-		}
-
-		ret_t operator()(args_t... args) const
-		{
-			if constexpr ( std::is_void_v<ret_t> )
-				mCaller(mCallable, std::forward<args_t>(args)...);
-			else
-				return mCaller(mCallable, std::forward<args_t>(args)...);
-		}
-	};
 }
 
 #endif // !_STD_EXT_CALLABLE_H_
