f00891a06c8be6eab80684979f096cf98a394974
Aaron Karp <aaron.m.karp@gmail.com>
2026-02-24 15:06:18 -0500

    Some platform stuff.

--- include/StdExt/Platform.h
+++ include/StdExt/Platform.h
@@ -5,10 +5,10 @@
 
 #if defined(__apple_build_version__)
 #	define STD_EXT_APPLE
-#elif defined(__clang__)
-#	define STD_EXT_CLANG
 #elif defined(_MSC_VER)
 #	define STD_EXT_WIN32
+#elif defined(__clang__)
+#	define STD_EXT_CLANG
 #else
 #	define STD_EXT_GCC
 #endif
@@ -23,44 +23,45 @@
 #	endif
 #endif
 
+#if defined(_MSC_VER)
+#	ifdef _CPPRTTI
+#		define STD_EXT_RTTI
+#	endif
+#elif defined(__clang__)
+#	if __has_feature(cxx_rtti)
+#		define STD_EXT_RTTI
+#	endif
+#else
+#	ifdef __GXX_RTTI
+#		define STD_EXT_RTTI
+#	endif
+#endif
+
 namespace StdExt
 {
 	namespace Platform
 	{
 		namespace Compiler
 		{
-		#ifdef _MSC_VER
-			static constexpr bool isVisualStudio = true;
-			static constexpr bool isGcc = false;
-			static constexpr bool isClang = false;
-			#ifdef _DEBUG
-				static constexpr bool debug_build = true;
-			#else
-				static constexpr bool debug_build = false;
-			#endif
-		#elif defined(__clang__)
+		#if defined(STD_EXT_APPLE)
 			static constexpr bool isVisualStudio = false;
 			static constexpr bool isGcc = false;
 			static constexpr bool isClang = true;
-			#ifdef __OPTIMIZE__
-				static constexpr bool debug_build = false;
-			#else
-				static constexpr bool debug_build = true;
-			#endif
+			static constexpr bool isApple = true;
+		#elif defined(STD_EXT_WIN32)
+			static constexpr bool isVisualStudio = true;
+			static constexpr bool isGcc = false;
+			static constexpr bool isClang = false;
+			static constexpr bool isApple = false;
+		#elif defined(STD_EXT_CLANG)
+			static constexpr bool isVisualStudio = false;
+			static constexpr bool isGcc = false;
+			static constexpr bool isClang = true;
+			static constexpr bool isApple = false;
 		#else
 			static constexpr bool isVisualStudio = false;
 			static constexpr bool isGcc = true;
 			static constexpr bool isClang = false;
-			#ifdef NDEBUG
-				static constexpr bool debug_build = false;
-			#else
-				static constexpr bool debug_build = true;
-			#endif
-		#endif
-
-		#ifdef __apple_build_version__
-			static constexpr bool isApple = true;
-		#else
 			static constexpr bool isApple = false;
 		#endif
 		}
@@ -72,6 +73,12 @@ namespace StdExt
 			static constexpr bool Debug = true;
 		#else
 			static constexpr bool Debug = false;
+		#endif
+
+		#if defined(STD_EXT_RTTI)
+			static constexpr bool RTTI = true;
+		#else
+			static constexpr bool RTTI = false;
 		#endif
 	}
 }
